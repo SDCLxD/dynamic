62e1f2f916e93500b2ac1602a7a411cc8265b9f9
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-10-02 21:22:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/api/auth', (req, res) => {
     return res.status(400).json({ message: 'Chave ou valor RNG ausente' });
   }
 
-  const rng_value = Math.floor(Math.random() * (1 - 2 + 1)) + 1;
+  const rng_value = Math.floor(Math.random() * (2009 - 27 + 1)) + 27;
   const modifiedRng = rng_value + 2
   console.log('Valor RNG recebido:', rng);
   console.log('Valor RNG modificado:', modifiedRng);
