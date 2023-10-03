1b7b153e73a8ec91368718f25448a10614125236
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-10-02 21:09:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-  host: '15.229.46.165',
+  host: '177.71.141.244',
   user: 'maquinaremota',
   password: 'senha123',
   database: 'sistemawhitelist'
