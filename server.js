2a533b65f9cd42362fce5057a142b5c7cd022257
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-10-02 22:34:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ db.connect((error) => {
 });
 
 app.post('/api/auth', (req, res) => {
-  const { chave } = req.query;
-  const { rng } = req.body;
+  const { chave, rng } = req.body;
   
   if (!chave) {
     return res.status(400).json({ message: 'Chave ou valor RNG ausente' });
